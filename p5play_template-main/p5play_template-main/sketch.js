8fc60d489ed05ec3af2e40fa4bec3d9653afc6fd
Kanav64 <93158904+Kanav64@users.noreply.github.com>
2021-11-28 10:27:34 +0000

    Add files via upload

---
+++ p5play_template-main/p5play_template-main/sketch.js
@@ -0,0 +1,24 @@
+var box
+function setup() {
+  createCanvas(400,400);
+  box=createSprite(100,100,10,10)
+}
+
+function draw() 
+{
+  
+  background(30);
+  if(keyDown("right")){
+  box.x=box.x+5}
+  if(keyDown("left")){
+    box.x=box.x-5}
+    if(keyDown("up")){
+      box.y=box.y-5}
+      if(keyDown("down")){
+        box.y=box.y+5}
+drawSprites()
+}
+
+
+
+
